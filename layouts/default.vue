16c9c6490197315f946dbec2eba22d350e218bb7
CZ17s <90894375+CZ17s@users.noreply.github.com>
2022-11-16 09:40:35 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,122 @@
+<template lang="pug">
+v-app
+  v-row
+    v-col(cols="1")
+      v-row.text-center.mt-7
+        v-col(cols="12")
+          v-app-bar-nav-icon.text-center.d-md-none(@click="drawer = true")
+          
+
+      v-row.text-center.d-none.d-md-flex
+        v-col(cols="12")
+          v-hover(v-for="(item, i) in dataButton" :key="i" v-slot="{ hover }")
+            v-avatar.mx-2(:color="item.color" elevation="2" :elevation="hover ? 12 : 2" size="24")
+              v-icon(v-if="hover" color="white") {{item.icon}}
+              v-icon(v-else color="white")
+        v-col(cols="12")    
+          v-list(flat)
+            v-list-item-group( color="primary")
+              v-list-item(v-for="(item, i) in items" :key="i")
+                v-icon.mx-auto.my-8(color="primary" size="30") {{item.icon}}
+
+    v-col(cols="11" height="64px")
+
+      v-row.mt-6
+        v-col(cols="11")
+          v-text-field.boxInput.mx-auto(label="Search" prepend-inner-icon="mdi-magnify" filled rounded dense)
+        v-col.text-center(cols="1")
+          v-avatar( color="primary" size="56")
+            img(src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John")
+
+      v-row
+        v-col(cols="12")
+          v-main
+            v-container
+              Nuxt
+
+  v-navigation-drawer(v-model="drawer" absolute temporary)
+    v-list(nav dense) 
+      v-list-item-group(v-model="group" active-class="deep-purple--text text--accent-4")
+
+        v-row.mt-6
+          v-col.text-center(cols="12")
+            v-hover(v-for="(item, i) in dataButton" :key="i" v-slot="{ hover }")
+              v-avatar.mx-2(:color="item.color" elevation="2" :elevation="hover ? 12 : 2" size="24")
+                v-icon(v-if="hover" color="white") {{item.icon}}
+                v-icon(v-else color="white")
+              
+        v-row.mx-auto    
+          v-col.mx-auto(cols="12")
+            v-list(flat)
+              v-list-item-group( color="primary")
+                v-list-item(v-for="(item, i) in items" :key="i")
+                  v-icon.mx-auto.my-8(color="primary" size="30") {{item.icon}}
+
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      dataButton: [
+        {
+          icon: 'mdi-close',
+          color: 'red',
+        },
+        {
+          icon: 'mdi-minus',
+          color: 'yellow',
+        },
+        {
+          icon: 'mdi-garage-open-variant',
+          color: 'green',
+        }
+      ],
+      items: [
+        {
+          icon: 'mdi-home',
+          title: ' ',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chat-outline',
+          title: ' ',
+          to: '/inspire'
+        },
+        {
+          icon: 'mdi-clock-outline',
+          title: ' ',
+          to: '/'
+        },
+        {
+          icon: 'mdi-account-outline',
+          title: ' ',
+          to: '/'
+        },
+        {
+          icon: 'mdi-cog-outline',
+          title: ' ',
+          to: '/'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+<style>
+.boxInput{
+  width: 400px;
+}
+.boxList {
+  width: 200px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template lang="pug">
+
+v-row.mx-start
+  v-col.mt-10(cols='12')
+    .text-h1.font-weight-regular {{showTime}}
+    .text-subtitle-1.font-weight-medium {{showDate}}
+
+  v-row
+    v-col.mt-10.text-center(cols='6' sm="6" md="2" v-for="(item, i) in cardIcon" :key="i")
+      v-card.ma-3.rounded-xl(:color="item.color" :elevation="10" width="100px" height="100px")
+        v-icon.mt-6(color="white" size="56" ) {{item.icon}}
+        .text-subtitle-1.font-weight-bold.mt-7 {{item.title}}
+
+  v-col.mt-10(cols='12')
+    .text-h6.font-weight-bold Hi Alex!
+    .text-h6.font-weight-light No upcoming meetings today
+
+
+
+</template>
+
+<script>
+import { DateTime } from 'luxon';
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      showTime: "",
+      showDate: "",
+      showLuxon: "",
+      cardIcon: [
+      {
+        icon: 'mdi-video',
+        title: 'Meething',
+        color: 'orange'
+      },
+      {
+        icon: 'mdi-plus-box',
+        title: 'Join',
+        color: 'blue'
+      },
+      {
+        icon: 'mdi-calendar-month-outline',
+        title: 'Shedule',
+        color: 'blue'
+      },
+      {
+        icon: 'mdi-arrow-up-box',
+        title: 'Share Screen',
+        color: 'blue'
+      }
+    ]
+    }
+  },
+  created() {
+    setInterval(this.getTime, 1000);
+  },
+  methods: {
+    getTime() {
+
+      const today = new Date();
+      const time = today.getHours() + " : " + today.getMinutes() ;
+      
+      this.showTime = time
+      this.showDate = DateTime.now().toLocaleString(DateTime.DATE_MED)
+
+      return this.showTime, this.showDate;
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
